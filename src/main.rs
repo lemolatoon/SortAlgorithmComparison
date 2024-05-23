dcc8fb1f0c23886d07bcc02eb8f233b062cfc2c9
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-05-23 16:50:32 +0900

    Add csv

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ pub mod counter;
 pub mod data;
 pub mod sorting;
 
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 use data::ArrayWrapper;
-use rand::distributions::Uniform;
-use rand::prelude::*;
 
 fn sort_evaluator<T: Clone + Ord + Eq>(
     array: &mut ArrayWrapper<T>,
@@ -14,7 +15,7 @@ fn sort_evaluator<T: Clone + Ord + Eq>(
     f(array);
     let end = start.elapsed();
     println!(
-        "{}.{:020} sec\nswap_counter: {},\tcompare_counter: {}",
+        "{}.{:025} sec\nswap_counter: {},\tcompare_counter: {}",
         end.as_secs(),
         end.subsec_nanos() / 1_000_000,
         array.counter().swap_counter(),
@@ -23,15 +24,28 @@ fn sort_evaluator<T: Clone + Ord + Eq>(
 }
 
 fn main() {
-    let n = 5000000;
-    let mut array = Vec::with_capacity(n);
-    let seed = 42;
-    let uniform = Uniform::from(u8::MIN..=u8::MAX);
-    let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
-    for _ in 0..n {
-        let value = uniform.sample(&mut rng);
-        array.push(value);
+    let reader = BufReader::new(File::open("./data-utf8.csv").unwrap());
+    let lines = reader
+        .lines()
+        .skip_while(|l| !l.as_ref().unwrap().starts_with('2'))
+        .collect::<Result<Vec<String>, _>>()
+        .unwrap();
+    let mut array = Vec::<u16>::with_capacity(lines.len());
+    let content = lines.join("\n");
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(content.as_bytes());
+
+    for r in rdr.records() {
+        let r = r.unwrap();
+        let Ok(value) = r.get(1).unwrap().parse::<f32>() else {
+            continue;
+        };
+        array.push((value * 10.0) as u16);
     }
+
+    println!("Data length: {}", array.len());
+
     let array = ArrayWrapper::new(array);
     array.counter().reset_counter();
     {
@@ -47,7 +61,7 @@ fn main() {
     {
         println!("Radix Sort!");
         let mut array = array.clone();
-        sort_evaluator(&mut array, sorting::radix);
+        sort_evaluator(&mut array, sorting::radix2);
     }
     {
         println!("Insertion Sort!");
